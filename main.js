3b844a803c94122c6cc366a0227fb32a77d6120d
Kamronbekprogramer <jumaboyevkamronbek05@gmail.com>
2024-03-16 03:26:50 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,113 @@
+// const form = document.getElementById("form");
+// const inputName = document.querySelector(".name-input");
+// const inputEmail = document.querySelector(".email-input");
+// const inputLink = document.querySelector(".link-input");
+// const inputMessage = document.querySelector(".message-input");
+// const cards = document.querySelector(".cards-group");
+
+// const DATA = [];
+
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     let newUser = {
+//         name: inputName.value,
+//         email: inputEmail.value,
+//         link: inputLink.value,
+//         message: inputMessage.value
+//     };
+//     DATA.push(newUser);
+//     createTable(DATA);
+
+//     inputName.value = "";
+//     inputEmail.value = "";
+//     inputLink.value = "";
+//     inputMessage.value = "";
+// });
+
+// function createTable(data) {
+//     cards.innerHTML = "";
+
+//     data.forEach((user, index) => {
+//         let div = document.createElement("div");
+//         let img = document.createElement("img")
+//         img.style.width = "150px";
+//         img.style.height = "150px";
+//         img.style.borderRadius = "50%",
+
+//         img.setAttribute("src", "images/photo_2024-03-16_03-07-28.jpg");
+//         div.classList.add("card");
+//         div.appendChild(img); 
+//         div.innerHTML += `
+//             <h2>${user.name}</h2>
+//             <h4>${user.email}</h4>
+//             <h4>${user.message}</h4>
+//             <h6>${user.link}</h6>
+//             <button class="btn-danger" onclick="deleteUser(${index})">Delete</button>
+//         `;
+//         cards.appendChild(div);
+//     });
+// }
+
+// function deleteUser(index) {
+//     DATA.splice(index, 1);
+//     createTable(DATA);
+// }
+
+// createTable(DATA);
+
+const form = document.getElementById("form");
+const inputName = document.querySelector(".name-input");
+const inputEmail = document.querySelector(".email-input");
+const inputLink = document.querySelector(".link-input");
+const inputMessage = document.querySelector(".message-input");
+const cards = document.querySelector(".cards-group");
+const DATA = [];
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (inputName.value && inputEmail.value && inputLink.value && inputMessage.value) { // Shartni tekshiramiz
+        let newUser = {
+            name: inputName.value,
+            email: inputEmail.value,
+            link: inputLink.value,
+            message: inputMessage.value
+        };
+        DATA.push(newUser);
+        createTable(DATA);
+
+        inputName.value = "";
+        inputEmail.value = "";
+        inputLink.value = "";
+        inputMessage.value = "";
+    } else {
+        alert("Iltimos, barcha maydonlarni to'ldiring!"); // Foydalanuvchiga xabarnoma chiqaramiz
+    }
+});
+
+function createTable(data) {
+    cards.innerHTML = "";
+
+    data.forEach((user, index) => {
+        let div = document.createElement("div");
+        let img = document.createElement("img");
+        img.setAttribute("src", "images/photo_2024-03-16_03-07-28.jpg");
+        div.classList.add("card");
+        div.appendChild(img);
+        div.innerHTML += `
+            <h2>${user.name}</h2>
+            <h4>${user.email}</h4>
+            <h4>${user.message}</h4>
+            <h6>${user.link}</h6>
+            <button class="btn-danger" onclick="deleteUser(${index})">Delete</button>
+        `;
+        cards.appendChild(div);
+    });
+}
+
+function deleteUser(index) {
+    DATA.splice(index, 1);
+    createTable(DATA);
+}
+
+createTable(DATA);
+
